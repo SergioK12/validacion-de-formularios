94efd8f30009fa450e5ba7fe0a7b81b4dc011eea
SergioK12 <sergioabd460@gmail.com>
2023-03-04 05:16:03 +0100

    Formulario terminado

--- lib/views/loginview.dart
+++ lib/views/loginview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validacion_de_formularios/ui/inputdecorations.dart';
 
 import '../widgets/widgets.dart';
 
@@ -18,15 +19,62 @@ class LoginView extends StatelessWidget {
                     const SizedBox(height: 10,),
                     Text("Login", style:  Theme.of(context).textTheme.headlineLarge,),
                     const SizedBox(height: 30,),
-                    const Text("Formulario")
+                    const _LoginForm(),
+
 
                   ],
                 )
-              )
+              ),
+              const SizedBox(height: 30),
+              const Text("¿Ha olvidado su contraseña?",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),)
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(child: Column(
+      children: [
+        TextFormField(
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecorations.authInputDecoration(
+            hinText: "Sergio@test.com",
+            labeltextt: "Correo electrónico",
+            prefixIcon: Icons.alternate_email_outlined
+          )
+        ),
+        const SizedBox(height: 30),
+        TextFormField(
+          autocorrect: false,
+          obscureText: true,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecorations.authInputDecoration(
+            hinText: "*********",
+            labeltextt: "Contraseña",
+            prefixIcon: Icons.lock_outline
+          )
+        ),
+        const SizedBox(height: 30),
+        MaterialButton(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          disabledColor: Colors.green,
+          elevation: 0,
+          color: Colors.purple,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 80,vertical: 15),
+            child: const  Text("Ingresar", style: TextStyle(color: Colors.white),),
+          ),
+          onPressed: (){},
+        )
+      ],
+    ));
   }
 }
