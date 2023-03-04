4d3efefa977875d422c0b7c00a1d913cf3e916b6
SergioK12 <sergioabd460@gmail.com>
2023-03-04 04:42:03 +0100

    Creacion de la vista

--- lib/views/loginview.dart
+++ lib/views/loginview.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('LoginView'),
+    return  Scaffold(
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children:   [
+               SizedBox(height: (MediaQuery.of(context).size.height * 0.30),),
+               CardContainer(
+                child: Column(
+                  children:  [
+                    const SizedBox(height: 10,),
+                    Text("Login", style:  Theme.of(context).textTheme.headlineLarge,),
+                    const SizedBox(height: 30,),
+                    const Text("Formulario")
+
+                  ],
+                )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'package:validacion_de_formularios/widgets/card_container.dart';
+
+export 'package:validacion_de_formularios/widgets/auth_background.dart';
+
